e9f1b8bd693821d679855cf8a6ac9ae7dd5851d5
daenur <daenur@outlook.com>
2023-10-21 16:28:42 +0300

    Update console.js

--- console.js
+++ console.js
@@ -1,26 +1,28 @@
+function findElementBySubClass(tagName, classSubStirng, last=false) {
+	const elementArr = Array.from( document.querySelectorAll(tagName) )
+	if (last) {
+		return elementArr.findLast(el=>el.classList.value.includes(classSubStirng))
+	}
+	return elementArr.find(el=>el.classList.value.includes(classSubStirng))
+}
+
+function getTimeNum() {
+	return +(findElementBySubClass('div','DivSeekBarTimeContainer')||{textContent:'0'}).textContent.split('/')[0].replace(':','')
+}
+
+function nextVideo() {
+	findElementBySubClass('button', 'ButtonBasicButtonContainer', true)
+	.click()
+}
+
+function hideComentBlock() {
+	findElementBySubClass('div', 'DivBrowserModeContainer').childNodes[1].style.display = 'none'
+	
+}
+
+hideComentBlock()
+
 function isAll(oldTimeNum=0, isAllFunction=undefined) {
-
-	function findElementBySubClass(tagName, classSubStirng, last=false) {
-		const elementArr = Array.from( document.querySelectorAll(tagName) )
-		if (last) {
-			return elementArr.findLast(el=>el.classList.value.includes(classSubStirng))
-		}
-		return elementArr.find(el=>el.classList.value.includes(classSubStirng))
-	}
-
-	function getTimeNum() {
-		return +(findElementBySubClass('div','DivSeekBarTimeContainer')||{textContent:'0'}).textContent.split('/')[0].replace(':','')
-	}
-
-	function nextVideo() {
-		Array.from(
-			document.querySelectorAll('button')
-		)
-		.findLast(button=>
-			button.classList.value.includes('ButtonBasicButtonContainer')
-		)
-		.click()
-	}
 
 	if (isAllFunction===undefined) {
 		isAllFunction = nextVideo
